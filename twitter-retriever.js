e997145542470f55dea8986a0a981bdb17928430
Pietro Avolio <pietro.avolio@mail.polimi.it>
2017-01-24 12:17:45 +0100

    Really fixed the bug about the tweets regarding N and M lines

--- twitter-retriever.js
+++ twitter-retriever.js
@@ -77,11 +77,9 @@ function parseTweet(tweet){
 	for(var i = 0; i < tweet.entities.hashtags.length; i++){
 		var hashtag = tweet.entities.hashtags[i];
 	
-		if(hashtag.text.startsWith("tram") || hashtag.text.startsWith("bus") || (new RegExp(/N\d+|M\d/)).test(hashtag)){
+		if(hashtag.text.startsWith("tram") || hashtag.text.startsWith("bus") || /N\d+|M\d/.test(hashtag.text)){
 			var line = hashtag.text.replace(/(bus|tram)/, "");
 			process.send({"line": line, "text": cleanText, "date": tweet.created_at, "issue-notification": !isStartup});
-		}else{
-			
 		}
 	}
 }
